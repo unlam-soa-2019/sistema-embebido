4d6ed83c8278e87edf6591b632a84837e8ae6605
Micaela Ramirez <ramirezmibe@gmail.com>
2019-06-09 19:58:26 -0300

    dispositivos emparejados

--- Android/app/src/main/java/com/example/smarttrashcan/DispositivosBluetooth.java
+++ Android/app/src/main/java/com/example/smarttrashcan/DispositivosBluetooth.java
@@ -1,22 +1,38 @@
 package com.example.smarttrashcan;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class DispositivosBluetooth extends AppCompatActivity {
 
-    ListView lista;
     private static final String tag = "DispositivosBluetooth";
+    public static final String extra_device_address = "device_address";
     private BluetoothAdapter bluetoothAdapter;
+    private ArrayAdapter dispositivosVinculados;
+
+    private BluetoothAdapter getBluetoothAdapter() {
+        if (bluetoothAdapter == null) {
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        }
+        return bluetoothAdapter;
+    }
 
     private void conectarBluetooth() {
         // chequea el bluetooth del dispositivo e intenta habilitarlo
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = getBluetoothAdapter();
         if (bluetoothAdapter == null) {
             Toast.makeText(getBaseContext(), "¡Ops! Este dispositivo no cuenta con conexión bluetooth", Toast.LENGTH_SHORT).show();
         } else {
@@ -29,12 +45,28 @@ public class DispositivosBluetooth extends AppCompatActivity {
         }
     }
 
+    private String getMACAdressFromView(View view) {
+        // Obtiene la dirección MAC del dispositivo, que son los últimos 17 caracteres en la vista
+        String viewText = ((TextView) view).getText().toString();
+        return viewText.substring(viewText.length() - 17);
+    }
+
+    private AdapterView.OnItemClickListener onClickDevice = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+            /*String address = getMACAdressFromView(v); algo no anda de esto, ver
+            Log.d(tag, "Dirección MAC obtenida: " + address);*/
+            // Realiza un intent para ir al menu de acciones, envía la MAC
+
+            Intent i = new Intent(getApplicationContext(), MenuAcciones.class);
+            //i.putExtra(extra_device_address, address);
+            startActivity(i);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispositivos_bluetooth);
-
-        lista = findViewById(R.id.listaDispositivos);
     }
 
     @Override
@@ -42,5 +74,17 @@ public class DispositivosBluetooth extends AppCompatActivity {
         super.onResume();
 
         conectarBluetooth();
+
+        dispositivosVinculados = new ArrayAdapter(this, R.layout.single_dispositivo, R.id.textView3);
+        // Presenta los dispositivos vinculados en el ListView
+        ListView listaDispositivos = findViewById(R.id.listaDispositivos);
+        listaDispositivos.setAdapter(dispositivosVinculados);
+        listaDispositivos.setOnItemClickListener(onClickDevice);
+
+        // Agrega a la lista los dispositivos previamente emparejados
+        Set<BluetoothDevice> pairedDevices = getBluetoothAdapter().getBondedDevices();
+        for (BluetoothDevice device : pairedDevices) {
+            dispositivosVinculados.add(device.getName() + "\n" + device.getAddress());
+        }
     }
 }
