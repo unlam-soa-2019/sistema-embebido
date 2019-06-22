0a13481eff63280a9e9b68149b14cda76bda17b9
ojedamarian <ojeda.e.mariana@gmail.com>
2019-06-22 18:51:05 -0300

    Update Proyecto.ino

--- Sistema-Embebido/Proyecto.ino
+++ Sistema-Embebido/Proyecto.ino
@@ -1,4 +1,8 @@
 #include <Servo.h>
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(4,3); // RX | TX
+
+char c = ' ';
 
 const int rele = 13;
 const int EchoPin = 5;
@@ -18,6 +22,14 @@ void setup() {
   pinMode(TriggerPin, OUTPUT);
   pinMode(EchoPin, INPUT);
   pinMode(rele,OUTPUT);
+
+    //Se configura la velocidad del puerto serie para poder imprimir en el puerto Serie
+    //Serial.begin(9600);
+    Serial.println("Inicializando configuracion del HC-05...");
+    
+    //Se configura la velocidad de transferencia de datos entre el Bluethoot  HC05 y el de Android.
+    BTserial.begin(9600); 
+    Serial.println("Esperando Comandos AT...");
 }
 
 void loop() {
@@ -29,8 +41,8 @@ void loop() {
   tiempo = (pulseIn(EchoPin, HIGH)/2); 
   distancia = float(tiempo * 0.0343);
 
-  Serial.print("Distancia: "); // imprime la distancia en el Monitor Serie
-  Serial.println(distancia);
+  //Serial.print("Distancia: "); // imprime la distancia en el Monitor Serie
+  //Serial.println(distancia);
    
   if ((distancia < 10) && bolsaAbierta) 
   {
@@ -40,8 +52,8 @@ void loop() {
     digitalWrite(rele, LOW);
     bolsaAbierta = false;
     servoMotor.write(90);
-    delay(5000);
-    seCambioBolsa = true;//sacar
+    //delay(5000);
+    //seCambioBolsa = true;//sacar
   }
 
   if (bolsaAbierta)
@@ -64,4 +76,24 @@ void loop() {
     bolsaAbierta = true;
     seCambioBolsa = false;
   }
+
+   //sI reciben datos del HC05 
+    if (BTserial.available())
+    { 
+        //se los lee y se los muestra en el monitor serie
+        c = BTserial.read();
+        Serial.write(c);
+        seCambioBolsa = true;
+        //Serial.println(c); 
+     }
+
+    //Si se ingresa datos por teclado en el monitor serie 
+    if (Serial.available())
+    {
+        //se los lee y se los envia al HC05
+        c =  Serial.read();
+        //Serial.write(c);
+        //Serial.println(c);
+        BTserial.write(c); 
+    }
 }
